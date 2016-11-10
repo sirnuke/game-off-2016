2183809cdb5459f077a8a43f915a208c1db9ab2c
Bryan DeGrendel <bryan@degrendel.com>
2016-11-10 01:38:39 -0500

    Implemented basic assignment calculating

--- game-off/engine/parser.lua
+++ game-off/engine/parser.lua
@@ -13,12 +13,22 @@ function Parser:init(debug)
   D = debug or false
   self.Grammar = Ct(P{
     "input",
-    input = P(-1) + ((V'whitespace'^0 * V'line')^1 * V'whitespace'^0 * -1),
-    line = V'statement'+V'comment',
-    statement = V'string',
-    string = (P'"' * C((P(1) - P'"')^0) * P'"') / self.destring,
+    input = P(-1) + ((V'whitespace' * V'line')^1 * V'whitespace' * -1),
+    line = V'expression' + V'comment',
+
     comment = P'--' * C((P(1) - P'\n')^0) * P'\n'^-1 / self.decomment,
-    whitespace = S' \t\r\n',
+
+    expression = V'assignment' + V'value',
+
+    assignment = V'symbol' * V'whitespace' * P'=' * V'whitespace' * V'expression' / self.deassignment,
+
+    value = V'string' + V'number',
+
+    string = (P'"' * C((P(1) - P'"')^0) * P'"') / self.destring,
+    number = C(R'09'^1 * P'.' * R'09'^0),
+
+    whitespace = S' \t\r\n'^0,
+    symbol = C((R'az' + R'AZ' + P'_') * (R'az' + R'AZ' + R'09' + P'_')^0),
   })
 end
 
@@ -34,4 +44,9 @@ end
 function Parser.decomment(contents)
   if D then Log.info(tag, "Comment! %s", contents) end
   return { "comment", contents }
+end
+
+function Parser.deassignment(variable, value)
+  if D then Log.info(tag, "Assignment! %s = %s", variable, Inspect(value)) end
+  return { "assignment", variable, value }
 end
