fae668c5aafbf1ff941d65d8c923512ace00ef66
Bryan DeGrendel <bryan@degrendel.com>
2016-11-12 17:38:12 -0500

    Cleanup string parsing and internal function names

--- game-off/engine/parser.lua
+++ game-off/engine/parser.lua
@@ -16,18 +16,18 @@ function Parser:init(debug)
     input = V'block'^0 * -1,
     block = (V'whitespace'^0 * (V'expression' + V'comment') * V'whitespace'^0)^1,
 
-    comment = P'--' * C((P(1) - P'\n')^0) * P'\n'^-1 / self.decomment,
+    comment = P'--' * C((P(1) - P'\n')^0) * P'\n'^-1 / self.tocomment,
 
     expression = V'whitespace'^0 * (V'assignment' + V'function_call' + V'symbol' + V'value'),
 
-    assignment = V'symbol' * V'whitespace'^0 * P'=' * V'whitespace'^0 * V'expression' / self.deassignment,
+    assignment = V'symbol' * V'whitespace'^0 * P'=' * V'whitespace'^0 * V'expression' / self.toassignment,
 
     value = V'string' + V'number',
 
-    function_call = V'symbol' * V'whitespace'^0 * P'(' * V'function_arguments'^0 * V'whitespace'^0 * P')' / self.defunctioncall,
+    function_call = V'symbol' * V'whitespace'^0 * P'(' * V'function_arguments'^0 * V'whitespace'^0 * P')' / self.tofunctioncall,
     function_arguments = C(V'whitespace'^0 * V'expression' * (V'whitespace'^0 * P',' * V'whitespace'^0 * V'expression')^0),
 
-    string = (P'"' * C((P(1) - P'"')^0) * P'"') / self.destring,
+    string = (P'"' * C((P(1) - P'"')^0) * P'"'),
     number = C(R'09'^1 * (P'.' * R'09')^0) / tonumber,
 
     whitespace = S' \t\r\n',
@@ -39,22 +39,17 @@ function Parser:parse(input)
   return self.Grammar:match(input)
 end
 
-function Parser.destring(contents)
-  if D then Log.info(tag, "String! %s", contents) end
-  return { "string", contents }
-end
-
-function Parser.decomment(contents)
+function Parser.tocomment(contents)
   if D then Log.info(tag, "Comment! %s", contents) end
   return { "comment", contents }
 end
 
-function Parser.deassignment(variable, value)
+function Parser.toassignment(variable, value)
   if D then Log.info(tag, "Assignment! %s = %s", variable, Inspect(value)) end
   return { "assignment", variable, value }
 end
 
-function Parser.defunctioncall(symbol, arguments)
+function Parser.tofunctioncall(symbol, arguments)
   if D then Log.info(tag, "Function call! %s ( %s )", symbol, Inspect(arguments)) end
   return { "function-call", symbol, arguments }
 end
