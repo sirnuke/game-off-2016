5c5c97eeb4ac947291ba6780806dcbbd5ada5040
Bryan DeGrendel <bryan@degrendel.com>
2016-11-12 18:02:52 -0500

    Implemented true/false values

--- game-off/engine/parser.lua
+++ game-off/engine/parser.lua
@@ -5,12 +5,12 @@ local tag = "Parser"
 
 local S, P, R, V, C, Ct = LPeg.S, LPeg.P, LPeg.R, LPeg.V, LPeg.C, LPeg.Ct
 
-local D
+local DEBUG
 
 Parser =  Class("Parser")
 
 function Parser:init(debug)
-  D = debug or false
+  DEBUG = debug or false
   self.Grammar = Ct(P{
     "input",
     input = V'block'^0 * -1,
@@ -18,17 +18,19 @@ function Parser:init(debug)
 
     comment = P'--' * C((P(1) - P'\n')^0) * P'\n'^-1 / self.tocomment,
 
-    expression = V'whitespace'^0 * (V'assignment' + V'function_call' + V'symbol' + V'value'),
+    expression = V'whitespace'^0 * (V'assignment' + V'function_call' + V'value' + V'symbol'),
 
     assignment = V'symbol' * V'whitespace'^0 * P'=' * V'whitespace'^0 * V'expression' / self.toassignment,
 
-    value = V'string' + V'number',
+    value = V'false_' + V'true_' + V'string' + V'number',
 
     function_call = V'symbol' * V'whitespace'^0 * P'(' * V'function_arguments'^0 * V'whitespace'^0 * P')' / self.tofunctioncall,
     function_arguments = C(V'whitespace'^0 * V'expression' * (V'whitespace'^0 * P',' * V'whitespace'^0 * V'expression')^0),
 
     string = (P'"' * C((P(1) - P'"')^0) * P'"'),
     number = C(R'09'^1 * (P'.' * R'09')^0) / tonumber,
+    false_ = (P'F'+P'f') * (P'A'+P'a') * (P'L'+P'l') * (P'S'+P's') * (P'E'+P'e') / self.tofalse,
+    true_ = (P'T'+P't') * (P'R'+P'r') * (P'U'+P'u') * (P'E'+P'e') / self.totrue,
 
     whitespace = S' \t\r\n',
     symbol = C((R'az' + R'AZ' + P'_') * (R'az' + R'AZ' + R'09' + P'_')^0),
@@ -40,16 +42,19 @@ function Parser:parse(input)
 end
 
 function Parser.tocomment(contents)
-  if D then Log.info(tag, "Comment! %s", contents) end
+  if DEBUG then Log.info(tag, "Comment! %s", contents) end
   return { "comment", contents }
 end
 
 function Parser.toassignment(variable, value)
-  if D then Log.info(tag, "Assignment! %s = %s", variable, Inspect(value)) end
+  if DEBUG then Log.info(tag, "Assignment! %s = %s", variable, Inspect(value)) end
   return { "assignment", variable, value }
 end
 
 function Parser.tofunctioncall(symbol, arguments)
-  if D then Log.info(tag, "Function call! %s ( %s )", symbol, Inspect(arguments)) end
+  if DEBUG then Log.info(tag, "Function call! %s ( %s )", symbol, Inspect(arguments)) end
   return { "function-call", symbol, arguments }
 end
+
+function Parser.totrue() return true end
+function Parser.tofalse() return false end
